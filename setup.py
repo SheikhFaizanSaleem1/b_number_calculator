6d987f1f2c64e3b274df0c79fce31b9e421a2285
sheikhfaizansaleem1 <fzsaleem786@gmail.com>
2022-09-09 12:32:41 +0500

    first commit

---
+++ setup.py
@@ -0,0 +1,12 @@
+import setuptools
+
+setuptools.setup(name='basic_ned_number_calculator',
+    version='0.0.1',
+    author='Sheikh Faizan',
+    author_email='fzsaleem786@gmail.com',
+    description='Basic Calculationg Package',
+    packages=setuptools.find_packages(),
+    classifiers=['Programming Language :: Python :: 3',
+    'License :: OSI Approved :: MIT License',
+    'Operating System :: OS Independent'],
+    )
